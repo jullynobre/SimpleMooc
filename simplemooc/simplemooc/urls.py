8ec667fa16ac7d3d581771aa68e9b03335cf1349
Jully Nobre <jullynobre1@gmail.com>
2017-11-24 13:11:24 -0300

    Correction in urls import

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from core import views
+from simplemooc.core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', simplemooc.core.views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 ]
